61d1a4f6172b504c84f1919430bf9007ba75d5d5
SabeloX <sabelomtw@gmail.com>
2023-05-22 00:51:09 +0200

    Added the header and search input field

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,104 @@
+"use client"
+
 import Image from 'next/image'
-import styles from './page.module.css'
+import { useState } from 'react'
+import { PT_Serif, PT_Sans, Roboto_Mono } from 'next/font/google'
+import { ActionIcon, Container, Divider, Flex, Switch, Text, TextInput } from '@mantine/core'
+import Colours from "../colours";
+
+const serif = PT_Serif({ subsets: ['latin'], weight: "400" });
+const sans = PT_Sans({ subsets: ['latin'], weight: "400" });
+const robotoMono = Roboto_Mono({ subsets: ['latin'], weight: "400" });
 
 export default function Home() {
+  const [fontFamily, setFontFamily] = useState<{
+    label: "Serif" | "Sans Serif" | "Roboto Mono",
+    value: any
+  }>({ label: "Serif", value: serif});
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <Container className={fontFamily.value}>
+      <header>
+        <Flex
+          justify="space-between"
+          align="center"
+          sx={{
+            padding: "30px"
+          }}
+        >
+          <Image
+            src="/book.png"
+            width={50}
+            height={50}
+            alt="logo"
+          />
+          <Flex
+            gap={20}>
+            <Flex
+              align="center"
+              gap={5}
+            >
+              <Text>{fontFamily.label}</Text>
+              <ActionIcon>
+                <Image
+                  src="/icons/dropdown.svg"
+                  width={20}
+                  height={20}
+                  alt="dropdown"
+                />
+              </ActionIcon>
+            </Flex>
+            <Divider orientation='vertical' />
+            <Flex
+              align="center"
+              gap={5}
+            >
+              <Switch />
+              <ActionIcon>
+                <Image
+                  src="/icons/dark.svg"
+                  width={20}
+                  height={20}
+                  alt="dropdown"
+                />
+              </ActionIcon>
+            </Flex>
+          </Flex>
+        </Flex>
+      </header>
+      <main>
+        <Flex
+          direction="column"
+          gap={30}
+        >
+          <Flex
+            justify="center"
+          >
+            <TextInput
+              styles={{
+                input: {
+                  borderRadius: "20px",
+                  backgroundColor: Colours.secondary,
+                  border: "none",
+                }
+              }}
+              sx={{
+                width: "100%"
+              }}
+              placeholder='Search a word'
+              rightSection={
+                <ActionIcon>
+                  <Image
+                    src="/icons/search.svg"
+                    width={20}
+                    height={20}
+                    alt="dropdown"
+                  />
+                </ActionIcon>
+              }
+            />
+          </Flex>
+        </Flex>
+      </main>
+    </Container>
   )
 }
---
+++ colours.ts
@@ -0,0 +1,7 @@
+export default {
+    primary: "#49535a",
+    secondary: "#f9f9fa",
+    text: "#090b0b",
+    background: "#e6eaea",
+    accent: "#1c1c22"
+}
